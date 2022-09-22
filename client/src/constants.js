b5fe85e952375c2d15d00f79bd5dab9396ac4348
Edwin <zizo_edwin@hotmail.com>
2022-09-22 23:03:57 +0800

    Changed web description , title and logo

--- client/src/constants.js
+++ client/src/constants.js
@@ -18,11 +18,9 @@ export const DEFAULT_QUERY_3 = "load";
 export const DEFAULT_QUERY_4 = "drive";
 export const DEFAULT_QUERY_5 = "uptime";
 export const CONTEC_QUERY_DRIVE = "drive_contec";
-export const DEFAULT_DEVICE = "d3f23b04aa74";
-// export const DEFAULT_DEVICE = "EC2AMAZ-5FRJVA8";
+export const DEFAULT_DEVICE = process.env.REACT_APP_DEFAULT_DEVICE;
 export const DEFAULT_CPU = "0";
-export const DEFAULT_DRIVE = "C:";
-export const DEFAULT_DRIVE_ROOT = "/";
+export const DEFAULT_DRIVE = process.env.REACT_APP_DEFAULT_DRIVE;
 export const DEFAULT_DRIVE_MMC = "/mnt/ext_mmc";
 /* STATIC PAGE */
 export const DEFAULT_GRAPH_TYPE = "line";
